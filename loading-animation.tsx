c550aeaad2ab781fd88c5310a3688d23e7f1d93d
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2026-01-28 16:16:36 -0600

    Update loading-animation.tsx

--- loading-animation.tsx
+++ loading-animation.tsx
@@ -223,6 +223,40 @@ export const LoadingAnimation = ({ size }: { size: number }) => {
             shadowRadius: 10,
           }}
         />
+        <MotiView
+          from={{
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            borderWidth: 1,
+            shadowOpacity: 0.5,
+            borderColor: "white",
+            shadowColor: "white",
+          }}
+          animate={{
+            width: size + 20,
+            height: size + 20,
+            borderRadius: (size + 20) / 2,
+            borderWidth: size / 10,
+            shadowOpacity: 1,
+            borderColor: "cyan",
+            shadowColor: "white",
+          }}
+          transition={{
+            type: "timing",
+            duration: 1000,
+            loop: true,
+          }}
+          style={{
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            borderWidth: size / 10,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 1,
+            shadowRadius: 10,
+          }}
+        />
       </View>
     </>
   );
